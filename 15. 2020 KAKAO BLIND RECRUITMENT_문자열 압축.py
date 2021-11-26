e40df0f6de68aaa23c6aec261b09fd37dd300abb
geon_hyeon <ais04134@naver.com>
2021-11-27 00:02:21 +0900

    no message

--- 15. 2020 KAKAO BLIND RECRUITMENT_문자열 압축.py
+++ 15. 2020 KAKAO BLIND RECRUITMENT_문자열 압축.py
@@ -1,84 +1,89 @@
 # -*- coding: utf-8 -*-
 '''
 
-문제 설명
-데이터 처리 전문가가 되고 싶은 "어피치"는 문자열을 압축하는 방법에 대해 공부를 하고 있습니다. 최근에 대량의 데이터 처리를 위한 간단한 비손실 압축 방법에 대해 공부를 하고 있는데, 문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현하여 더 짧은 문자열로 줄여서 표현하는 알고리즘을 공부하고 있습니다.
-간단한 예로 "aabbaccc"의 경우 "2a2ba3c"(문자가 반복되지 않아 한번만 나타난 경우 1은 생략함)와 같이 표현할 수 있는데, 이러한 방식은 반복되는 문자가 적은 경우 압축률이 낮다는 단점이 있습니다. 예를 들면, "abcabcdede"와 같은 문자열은 전혀 압축되지 않습니다. "어피치"는 이러한 단점을 해결하기 위해 문자열을 1개 이상의 단위로 잘라서 압축하여 더 짧은 문자열로 표현할 수 있는지 방법을 찾아보려고 합니다.
-
-예를 들어, "ababcdcdababcdcd"의 경우 문자를 1개 단위로 자르면 전혀 압축되지 않지만, 2개 단위로 잘라서 압축한다면 "2ab2cd2ab2cd"로 표현할 수 있습니다. 다른 방법으로 8개 단위로 잘라서 압축한다면 "2ababcdcd"로 표현할 수 있으며, 이때가 가장 짧게 압축하여 표현할 수 있는 방법입니다.
-
-다른 예로, "abcabcdede"와 같은 경우, 문자를 2개 단위로 잘라서 압축하면 "abcabc2de"가 되지만, 3개 단위로 자른다면 "2abcdede"가 되어 3개 단위가 가장 짧은 압축 방법이 됩니다. 이때 3개 단위로 자르고 마지막에 남는 문자열은 그대로 붙여주면 됩니다.
-
-압축할 문자열 s가 매개변수로 주어질 때, 위에 설명한 방법으로 1개 이상 단위로 문자열을 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이를 return 하도록 solution 함수를 완성해주세요.
-
-제한사항
-s의 길이는 1 이상 1,000 이하입니다.
-s는 알파벳 소문자로만 이루어져 있습니다.
-입출력 예
-s	result
-"aabbaccc"	7
-"ababcdcdababcdcd"	9
-"abcabcdede"	8
-"abcabcabcabcdededededede"	14
-"xababcdcdababcdcd"	17
-입출력 예에 대한 설명
-입출력 예 #1
-
-문자열을 1개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #2
-
-문자열을 8개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #3
-
-문자열을 3개 단위로 잘라 압축했을 때 가장 짧습니다.
-
-입출력 예 #4
-
-문자열을 2개 단위로 자르면 "abcabcabcabc6de" 가 됩니다.
-문자열을 3개 단위로 자르면 "4abcdededededede" 가 됩니다.
-문자열을 4개 단위로 자르면 "abcabcabcabc3dede" 가 됩니다.
-문자열을 6개 단위로 자를 경우 "2abcabc2dedede"가 되며, 이때의 길이가 14로 가장 짧습니다.
-
-입출력 예 #5
-
-문자열은 제일 앞부터 정해진 길이만큼 잘라야 합니다.
-따라서 주어진 문자열을 x / ababcdcd / ababcdcd 로 자르는 것은 불가능 합니다.
-이 경우 어떻게 문자열을 잘라도 압축되지 않으므로 가장 짧은 길이는 17이 됩니다.
+문제출처: https://programmers.co.kr/learn/courses/30/lessons/60057
 
 '''
 # 으어어 문제를 잘 못 이해하고 못 풀어버렸다..
 s = "abcabcabcabcdededededede"
 
-def solution(s):
-    compressed_text_fragments = []
+# def solution(s):
+#     compressed_text_fragments = []
+#
+#     for i in range(len(s)):
+#         if s[:i+1] == s[i+1:2*(i+1)]:
+#             number_of_repetitions = 1
+#
+#             for j in range(len(s)//(len(s[:i+1])+i)):
+#                 if s[:i+1] == s[(i+1)*(j+1):(i+1)*(j+2)]:
+#                     number_of_repetitions += 1
+#                 elif s[(i+1)*(j+1):(i+1)*(j+2)] == s[(i+1)*(j+2):(i+1)*(j+3)]:
+#
+#                 else:
+#                     if number_of_repetitions == 1:
+#                         compressed_text_fragments.append(s[:i+1] + s[(i+1) * (j+1):])
+#                     else:
+#                         compressed_text_fragments.append(str(number_of_repetitions) + s[:i+1] + s[(i+1) * (j+1):])
+#
+#     if compressed_text_fragments == []:
+#         return len(s)
+#
+#     answer = min(list(map(len, compressed_text_fragments)))
+#
+#     return answer
+#
+# print(len("4abcdededededede"))
+#
+#
+# print(solution(s))
 
-    for i in range(len(s)):
-        if s[:i+1] == s[i+1:2*(i+1)]:
-            number_of_repetitions = 1
+# s = "abcabcabcabcdededededede"
+s = "aaaakkaaaakkaaaakk"
 
-            for j in range(len(s)//(len(s[:i+1])+i)):
-                if s[:i+1] == s[(i+1)*(j+1):(i+1)*(j+2)]:
-                    number_of_repetitions += 1
-                elif s[(i+1)*(j+1):(i+1)*(j+2)] == s[(i+1)*(j+2):(i+1)*(j+3)]:
+# def solution(s: str)-> int:
+#     answer = ""
+#     number = 1
+#     while True:
+#         for i in range(len(s)//2):
+#             if s[0:i+1] == s[i+1:2*(i+1)]:
+#                 number += 1
+#                 s = s[i+1:]
+#                 if s[0:i+1] != s[i+1:2*(i+1)]:
+#                     answer += f"{number}"+s[0:i+1]
+#                     number = 1
+#                     s = s[i+1:]
+#                 break
+#         if s == "":
+#             return answer
+#             break
+#
+#
+# print(solution(s))
 
-                else:
-                    if number_of_repetitions == 1:
-                        compressed_text_fragments.append(s[:i+1] + s[(i+1) * (j+1):])
-                    else:
-                        compressed_text_fragments.append(str(number_of_repetitions) + s[:i+1] + s[(i+1) * (j+1):])
+string = "kbdgbdgsdksdkkkk"
 
-    if compressed_text_fragments == []:
-        return len(s)
+def solution(string: str)-> int:
+    answer_list = []
+    for i in range((len(string)//2)+1): # i개씩 자르는 경우들 n/2이상은 의미가 없다.# 문자열 길이가 1인 경우가 있을 수 있으므로 +1 해준다.
+        number = 1
+        s = string
+        answer = ""
+        for j in range((len(string))): # i개씩 문자열을 자르는 작업!
+            if s[0:i + 1] == s[i + 1:2 * (i + 1)]: # 만약 자른 문자열이 이후 그마만큼의 문자열과 같을 경우
+                number += 1
+                s = s[i+1:]
+            else: # 자른 문자열이 이후 문자열과 다를 경우
+                if number == 1: # 반복이 없었을시 값 그대로 더하기
+                    answer += s[0:i + 1]
+                    s = s[i+1:]
+                else: # 반복이 있었을시 반복 횟수만큼 숫자를 더한 후 값 더하기
+                    answer += f"{number}" + s[0:i + 1]
+                    s = s[i+1:]
+                    number = 1
 
-    answer = min(list(map(len, compressed_text_fragments)))
+        answer_list.append(len(answer)) # i개씩 문자열을 잘랐을때의 결과값의 길이를 answer_list에 저장
 
-    return answer
-
-print(len("4abcdededededede"))
+    return min(answer_list) # 그중 최소값을 출력
 
 
-print(solution(s))
-
-
+print(solution(string))
