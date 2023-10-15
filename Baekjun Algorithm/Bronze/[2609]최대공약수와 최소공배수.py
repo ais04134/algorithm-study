78c25a698a58b1b7c912b483796f003fa84b7ae7
Geon <is04133333@gmail.com>
2023-10-15 13:29:00 +0900

    디폴트 변경 목록

--- Baekjun Algorithm/Bronze/[2609]최대공약수와 최소공배수.py
+++ Baekjun Algorithm/Bronze/[2609]최대공약수와 최소공배수.py
@@ -1,11 +1,31 @@
-import sys
-from collections import deque
+'''
+출처: https://www.acmicpc.net/problem/2609
+'''
 
-input = sys.stdin.readline
-n = deque(range(1, int(input())+1,))
 
-while len(n) != 1:
-    n.popleft()
-    n.rotate(-1)
+'''
+1. 유클리드 호제법
+'''
+a, b = map(int, input().split())
 
-print(n[0])
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+print(gcd(a, b))
+print(lcm(a, b))
+
+
+'''
+2. math 모듈 활용.
+'''
+# import math
+
+# a, b = map(int, input().split())
+
+# print(math.gcd(a, b))
+# print(math.lcm(a, b))
