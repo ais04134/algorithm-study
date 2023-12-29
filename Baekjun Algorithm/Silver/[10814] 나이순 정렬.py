19fe727f24b13602b4a6f51bed152ce9f1b17eb0
Geon <is04133333@gmail.com>
2023-12-29 19:22:54 +0900

    [10814] 나이순 정렬

---
+++ Baekjun Algorithm/Silver/[10814] 나이순 정렬.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+user = []
+
+for _ in range(N):
+    age, name = input().split()
+    user.append([int(age),name])
+
+for i in sorted(user, key = lambda x: x[0]):
+    print(i[0], i[1])
