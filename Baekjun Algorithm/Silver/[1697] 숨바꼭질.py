b35a263bc3d5564119a8c954494583c48b5d31b4
Geon <is04133333@gmail.com>
2024-01-10 14:58:50 +0900

    [1697] 숨바꼭질

---
+++ Baekjun Algorithm/Silver/[1697] 숨바꼭질.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    visited[start] = 1
+    while q:
+        c = q.popleft()
+        if c == k:
+            return visited[c] - 1
+        for i in (c-1, c+1, 2*c):
+            if 0 <= i <= 100000 and not visited[i]:
+                visited[i] = visited[c] + 1
+                q.append(i)
+
+n, k = map(int, input().split())
+visited = [0 for _ in range(100001)]
+
+print(bfs(n))
