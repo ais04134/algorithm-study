61832bfb5795680657821ce8598d51af8cadb7e4
Geon <is04133333@gmail.com>
2024-07-02 11:36:58 +0900

    [9095] 1.2.3 더하기

---
+++ Baekjun Algorithm/Silver/[9095] 1.2.3 더하기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+def func(x):
+  if x==1: return 1
+  elif x==2: return 2
+  elif x==3: return 4
+  else: return func(x-1)+func(x-2)+func(x-3)
+
+for _ in range(t):
+  n = int(input())
+  
+  print(func(n))
